ee62bf84bd0abbb3e99181640e2c7b1f18eca2b9
darius t <Darius.Taplin@sdgku.edu>
2024-04-09 10:33:45 -0700

    password

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
